45ba59f79ec712594060f508684d4747ed6c3336
Joe DeFranco <jdefranco449@gmail.com>
2021-01-11 20:16:15 -0500

    updated files

--- Develop/server.js
+++ Develop/server.js
@@ -5,6 +5,9 @@ const bodyParser = require("body-parser");
 const apiRoutes = require("./routes/api");
 const viewRoutes = require("./routes/views");
 
+const app = express();
+const PORT = process.env.PORT || 3000;
+
 mongoose.connect(
     process.env.MONGODB_URI || 'mongodb+srv://revis24:Apollo1215!@cluster0.k26th.mongodb.net/fitness_tracker?retryWrites=true&w=majority',
     {
@@ -14,3 +17,19 @@ mongoose.connect(
       useFindAndModify: false
     }
   );
+
+  
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(express.static("public"));
+
+app.use(apiRoutes);
+app.use(viewRoutes);
+// app.use(workoutRoutes);
+
+app.get("*", function(req, res) {
+  res.sendFile(path.join(__dirname, "./public/index.html"));
+});
+
+app.listen(PORT, () => console.log("listening on port: ", PORT));
