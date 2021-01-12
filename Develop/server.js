93af4b33f2ed790311e9937bfa8be297d749d85c
Joe DeFranco <jdefranco449@gmail.com>
2021-01-11 20:11:23 -0500

    update

--- Develop/server.js
+++ Develop/server.js
@@ -1,5 +1,12 @@
+const express = require("express");
+const path = require("path");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const apiRoutes = require("./routes/api");
+const viewRoutes = require("./routes/views");
+
 mongoose.connect(
-    process.env.MONGODB_URI || 'mongodb://localhost/fitness_tracker',
+    process.env.MONGODB_URI || 'mongodb+srv://revis24:Apollo1215!@cluster0.k26th.mongodb.net/fitness_tracker?retryWrites=true&w=majority',
     {
       useNewUrlParser: true,
       useUnifiedTopology: true,
