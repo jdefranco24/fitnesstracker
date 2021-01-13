837fbf2876beb22fb42b7323e4bd9670cfd707ec
Joe DeFranco <jdefranco449@gmail.com>
2021-01-12 20:07:52 -0500

    updated

---
+++ server.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const path = require("path");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const apiRoutes = require("./routes/api");
+const viewRoutes = require("./routes/views");
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+mongoose.connect(
+    process.env.MONGODB_URI || 'fitness_tracker',
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+      useFindAndModify: false
+    }
+  );
+
+  
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(express.static("public"));
+
+app.use(apiRoutes);
+app.use(viewRoutes);
+// app.use(workoutRoutes);
+
+app.get("*", function(req, res) {
+  res.sendFile(path.join(__dirname, "./public/index.html"));
+});
+
+app.listen(PORT, () => console.log("listening on port: ", PORT));
